a34b7bf575d70159a152835b81ca235399f6115a
Andrew <ava7@live.co.uk>
2022-02-20 23:32:14 +0000

    I am trying to upload this. 3rd time's a charm!

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.random.normal(-1, 1, 500)
+y = np.random.normal(-1, 1, 500)
+
+fig, ax = plt.subplots()
+ax.scatter(x, y, 50, "0.0", lw=2)
+ax.scatter(x, y, 50, "1.0", lw=0)
+ax.scatter(x, y, 40, "C1", lw=0, alpha=0.1)
+plt.show()
+
